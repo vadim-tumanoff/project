d44732eb0fe0f721537beb2b07ed307fe0662ba3
Vadim <vadim_tumanoff@mail.ru>
2022-06-19 23:41:28 +0800

    new task

--- script.js
+++ script.js
@@ -6,10 +6,31 @@ console.log(result) */
 /* const answer = prompt("Ты долбаеб?", "Нет");
 console.log(typeof(answer));  */
 
-const answers = [];
-
-answers[0] = prompt('Как тебя зовут?','');
+/* const answers = [];
+ */
+/* answers[0] = prompt('Как тебя зовут?','');
 answers[1] = prompt('Как ваша фамилия?','');
-answers[2] = prompt('Сколько тебе лет?','');
+answers[2] = prompt('Сколько тебе лет?',''); */
 
-document.write(answers);
+/* console.log(typeof(answers));
+ */
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?",""),
+      b = prompt("Насколько оцените его?",""),
+      c = prompt("Один из последних просмотренных фильмов?",""),
+      d = prompt("Насколько оцените его?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
