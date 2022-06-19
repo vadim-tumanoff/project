8590d3fa4fc2cc1b73e059613147fc737a51d0e3
Vadim <vadim_tumanoff@mail.ru>
2022-06-19 12:19:47 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use sctrict";
+
+/* const result = confirm("Are you here?");
+console.log(result) */
+
+/* const answer = prompt("Ты долбаеб?", "Нет");
+console.log(typeof(answer));  */
+
+const answers = [];
+
+answers[0] = prompt('Как тебя зовут?','');
+answers[1] = prompt('Как ваша фамилия?','');
+answers[2] = prompt('Сколько тебе лет?','');
+
+document.write(answers);
